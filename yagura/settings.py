2b6a5cf7001a92211615b1c77a15713ecba0f0f5
Kazuya Takei <attakei@gmail.com>
2018-06-27 02:34:42 +0900

    Define layouts

--- yagura/settings.py
+++ yagura/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'yagura', 'static'),
+]
 
 # django-bootstrap4
 BOOTSTRAP4 = {
