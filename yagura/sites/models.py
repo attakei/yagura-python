211579d0d9b28a4d85ce9fc5dc1b922ecefa5578
Kazuya Takei <attakei@gmail.com>
2018-06-28 14:50:12 +0900

    Define site model

--- yagura/sites/models.py
+++ yagura/sites/models.py
@@ -1,3 +1,22 @@
+from uuid import uuid4
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Site(models.Model):
+    """Monitoring target site model
+    """
+    id = models.UUIDField(
+        'Site ID in project', primary_key=True, default=uuid4)
+    url = models.URLField('Site URL')
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.url
+
+    def get_absolute_url(self):
+        return f"/sites/{self.id}"
--- yagura/sites/tests.py
+++ yagura/sites/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from parameterized import parameterized
 
-# Create your tests here.
+from yagura.sites.models import Site
+
+
+class Site_ModelTest(TestCase):
+    @parameterized.expand([
+        (
+            '443af180-4086-402d-8222-1f6aa5522993',
+            '/sites/443af180-4086-402d-8222-1f6aa5522993'),
+        (
+            '90585392-7e20-4b36-8020-01da52078427',
+            '/sites/90585392-7e20-4b36-8020-01da52078427'),
+    ])
+    @override_settings(ROOT_URLCONF='yagura.sites.tests')
+    def test_url(self, site_id, site_url):
+        site = Site(id=site_id)
+        assert site.get_absolute_url() == site_url
