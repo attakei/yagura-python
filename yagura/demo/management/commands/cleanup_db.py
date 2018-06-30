58613d0b21cf5092eba92f2d9f035c8da56594e7
Kazuya Takei <attakei@gmail.com>
2018-06-30 17:58:51 +0900

    Refactor command logic to use command internal

--- yagura/demo/management/commands/cleanup_db.py
+++ yagura/demo/management/commands/cleanup_db.py
@@ -1,21 +1,15 @@
-from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
-
-from yagura.monitors.models import StateHistory
-from yagura.sites.models import Site
 
 
 class Command(BaseCommand):
     help = 'Clear all application db and init users'
 
     def handle(self, *args, **options):
-        StateHistory.objects.all().delete()
-        Site.objects.all().delete()
-        self._cleanup_users()
+        self._run_command('flush', '--noinput')
+        self._run_command('loaddata', 'initial')
 
-    def _cleanup_users(self):
-        get_user_model().objects.all().delete()
-        call_command(
-            'loaddata', 'initial',
-            stdout=self.stdout, stderr=self.stderr)
+    def _run_command(self, *args):
+        """Shortcut of call_command that bind stdout and stderr
+        """
+        call_command(*args, stdout=self.stdout, stderr=self.stderr)
