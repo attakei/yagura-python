3a497d303e32242883ca548b92b45a272cc0fbd9
Kazuya Takei <attakei@gmail.com>
2018-06-30 21:00:22 +0900

    Define monitoring cronjob

--- yagura/settings/demo.py
+++ yagura/settings/demo.py
@@ -2,4 +2,5 @@ from yagura.settings.base import *
 
 CRONJOBS = [
     ('5 0 * * SUN', 'django.core.management.call_command', ['cleanup_db']),
+    ('*/10 * * * *', 'django.core.management.call_command', ['monitor_all']),
 ]
