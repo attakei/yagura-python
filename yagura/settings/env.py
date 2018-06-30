f3b9675efd2ae068fd55e0e199b2d12a0c724fd7
Kazuya Takei <attakei@gmail.com>
2018-07-01 00:52:00 +0900

    Update environ settings

--- yagura/settings/env.py
+++ yagura/settings/env.py
@@ -10,3 +10,20 @@ import environ
 from yagura.settings.base import *
 
 root = environ.Path(__file__) - 3
+env = environ.Env(
+    DEBUG=(bool, False),
+)
+
+# System
+DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
+DATABASES = {
+    'default': env.db(),
+}
+
+# django-registration
+ACCOUNT_ACTIVATION_DAYS = env.int('YAGURA_ACTIVATION_DAYS', default=7)
+
+# Yagura app
+YAGURA_BASE_URL = env.url('YAGURA_BASE_URL', 'http://localhost:8000')
+YAGURA_DEMO = env('YAGURA_DEMO', default=False)
