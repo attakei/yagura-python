73827cb8dfc549abe39386344eb56135f3da140d
Kazuya Takei <attakei@gmail.com>
2018-07-07 12:52:16 +0900

    First i18n settings

--- yagura/settings/base.py
+++ yagura/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from django.utils.translation import gettext_lazy as _
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -127,6 +129,15 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'yagura/locale'),
+]
+
+LANGUAGES = [
+    ('ja', _('Japanese')),
+    ('en', _('English')),
+]
 
 
 # Static files (CSS, JavaScript, Images)
