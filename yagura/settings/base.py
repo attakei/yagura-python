445268d534b8473448cae491aee7748a43125509
Kazuya Takei <attakei@gmail.com>
2018-07-07 20:01:19 +0900

    Load i18n templatetag always

--- yagura/settings/base.py
+++ yagura/settings/base.py
@@ -77,6 +77,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'django.templatetags.i18n',
+            ],
         },
     },
 ]
