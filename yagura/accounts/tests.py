cd9bda2351baef1c19348cf1aea54ee9dc0f3a36
Kazuya Takei <attakei@gmail.com>
2018-07-07 12:01:36 +0900

    Implement profie editing(name and email)

--- yagura/accounts/tests.py
+++ yagura/accounts/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
 
 from yagura.tests.base import ViewTestCase
 
@@ -16,4 +17,36 @@ class Profile_ViewTest(ViewTestCase):
     def test_logged_in(self):
         self.client.force_login(get_user_model().objects.first())
         resp = self.client.get(self.url)
+        assert resp.status_code == 200
+
+
+class ProfileEdit_ViewTest(ViewTestCase):
+    fixtures = [
+        'initial',
+    ]
+    url = reverse_lazy('accounts:profile-edit')
+
+    def test_login_required(self):
+        resp = self.client.get(self.url)
+        assert resp.status_code == 302
+
+    def test_logged_in_get(self):
+        self.client.force_login(get_user_model().objects.first())
+        resp = self.client.get(self.url)
+        assert resp.status_code == 200
+
+    def test_update_first_name(self):
+        user = get_user_model().objects.first()
+        self.client.force_login(user)
+        before_ = user.first_name
+        resp = self.client.post(
+            self.url, {'first_name': 'test', 'email': 'test@example.com'})
+        assert resp.status_code == 302
+        after_ = get_user_model().objects.first().first_name
+        assert before_ != after_
+
+    def test_email_require(self):
+        user = get_user_model().objects.first()
+        self.client.force_login(user)
+        resp = self.client.post(self.url, {'first_name': 'test'})
         assert resp.status_code == 200
--- yagura/accounts/views.py
+++ yagura/accounts/views.py
@@ -1,6 +1,27 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
+
+from yagura.accounts.forms import ProfileEditForm
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/profile.html'
+
+
+class ProfileEditView(LoginRequiredMixin, FormView):
+    form_class = ProfileEditForm
+    template_name = 'accounts/profile_edit.html'
+    success_url = reverse_lazy('accounts:profile')
+
+    def get_initial(self):
+        user = self.request.user
+        fields = self.form_class.Meta.fields
+        return {k: getattr(user, k) for k in fields}
+
+    def form_valid(self, form):
+        user = self.request.user
+        for k in self.form_class.Meta.fields:
+            setattr(user, k, form.cleaned_data[k])
+        user.save()
+        return super().form_valid(form)
