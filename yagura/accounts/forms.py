818f34eb4207ac28c7c6e062c6eb6da03f311131
Kazuya Takei <attakei@gmail.com>
2018-07-08 16:50:57 +0900

    User can set language by profile

--- yagura/accounts/forms.py
+++ yagura/accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.utils.translation import get_language
 
 
 class ProfileEditForm(forms.ModelForm):
@@ -10,3 +12,15 @@ class ProfileEditForm(forms.ModelForm):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.fields['email'].required = True
+
+
+# TODO: Not test
+class SetLanguageForm(forms.Form):
+    next = forms.URLField(widget=forms.HiddenInput)
+    language = forms.ChoiceField(
+        widget=forms.Select(attrs={'class': 'form-control-lg'}),
+        choices=settings.LANGUAGES)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['language'].initial = get_language()
