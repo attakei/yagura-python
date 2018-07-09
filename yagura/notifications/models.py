99959c8fa130a2f19ce78e808f4589792c00c48b
Kazuya Takei <attakei@gmail.com>
2018-07-09 14:55:11 +0900

    Define model ExtraRecipient

--- yagura/notifications/models.py
+++ yagura/notifications/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from yagura.sites.models import Site
+
+
+class ExtraRecipient(models.Model):
+    """Other recipients not users
+
+    * Manage per recipient and target site
+    """
+    site = models.ForeignKey(
+        Site, on_delete=models.CASCADE, related_name='extra_recipients')
+    email = models.EmailField()
+    enabled = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = (
+            ('site', 'email'),
+        )
