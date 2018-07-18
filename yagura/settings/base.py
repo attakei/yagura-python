f73aa1d79ff47a584e702df284ae6b489a520964
Kazuya Takei <attakei@gmail.com>
2018-07-19 00:50:45 +0900

    Distribute templates for each applications from shared template folder

--- yagura/settings/base.py
+++ yagura/settings/base.py
@@ -70,9 +70,7 @@ ROOT_URLCONF = 'yagura.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'yagura', 'templates'),
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
