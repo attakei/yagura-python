bb450a992afaaa50284a62d42ec7e4bd8d237fae
Kazuya Takei <attakei@gmail.com>
2018-07-19 01:32:12 +0900

    Move sources that belong to yagura.core

--- yagura/core/tests.py
+++ yagura/core/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class FixtureTest(TestCase):
+    fixtures = ['initial', ]
+
+    def test_password(self):
+        User = get_user_model()
+        user = User.objects.first()
+        assert user.check_password('Yagura!!')
+
+
+class IndexTest(TestCase):
+    def test_has_title_tag(self):
+        client = Client()
+        resp = client.get('/')
+        assert '<title>Yagura' in str(resp.content)
