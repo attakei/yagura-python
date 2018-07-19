f73541607d83f05081cc78fc6d9c1977b0336a5b
Kazuya Takei <attakei@gmail.com>
2018-07-19 23:43:10 +0900

    User can handle timezone

--- yagura/accounts/urls.py
+++ yagura/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from yagura.accounts.views import ProfileEditView, ProfileView
+from yagura.accounts.views import ProfileEditView, ProfileView, SetTimezoneView
 
 app_name = 'accounts'
 urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
     path('profile/edit', ProfileEditView.as_view(), name='profile-edit'),
+    path('profile/timezone', SetTimezoneView.as_view(), name='set-timezone'),
 ]
