feb6b99274ceb96b6ad11686ed833f62255034c6
Kazuya Takei <attakei@gmail.com>
2018-08-16 01:26:48 +0900

    Set version to template (footer)

--- yagura/core/context_processors.py
+++ yagura/core/context_processors.py
@@ -1,5 +1,12 @@
 from django.conf import settings
 
+from yagura import __version__
+
+
+def yagura_version(request):
+    return {
+        'YAGURA_VERSION': __version__ 
+    }
 
 def yagura_conf(request):
     return {
