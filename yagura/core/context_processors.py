0050e975133b47190b56d76b3e4a2c1b02f81c9d
Kazuya Takei <attakei@gmail.com>
2018-08-16 11:50:45 +0900

    Fix for flake8

--- yagura/core/context_processors.py
+++ yagura/core/context_processors.py
@@ -5,8 +5,9 @@ from yagura import __version__
 
 def yagura_version(request):
     return {
-        'YAGURA_VERSION': __version__ 
+        'YAGURA_VERSION': __version__
     }
+
 
 def yagura_conf(request):
     return {
