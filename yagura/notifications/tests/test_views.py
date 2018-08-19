27f513f23d1d13a9dc155f90ddddf1d7847654f5
Kazuya Takei <attakei@gmail.com>
2018-08-20 02:48:02 +0900

    Split test cases of yagura.notifications

--- yagura/notifications/tests/test_views.py
+++ yagura/notifications/tests/test_views.py
@@ -1,15 +1,11 @@
-from unittest import mock
-
 from django.contrib.auth import get_user_model
 from django.contrib.messages import get_messages
 from django.core import mail
-from django.test import TestCase
 from django.urls import reverse_lazy
 
 from yagura.notifications.models import (
     EmailActivation, EmailDeactivation, EmailRecipient, SlackRecipient
 )
-from yagura.notifications.services import SlackNotifier
 from yagura.sites.models import Site
 from yagura.tests.base import ViewTestCase
 
@@ -108,40 +104,6 @@ class Deactivate_ViewTest(ViewTestCase):
         assert EmailRecipient.objects.count() == 0
         resp = self.client.get(resp['Location'])
         assert resp.status_code == 200
-
-
-class SlackNotification_ModelTest(TestCase):
-    fixtures = [
-        'unittest_suite',
-    ]
-
-    def test_notification(self):
-        site = Site.objects.first()
-        recipient = SlackRecipient.objects.create(
-            site=site, url='http://example.com')
-        current_state = mock.MagicMock(
-            site=site, state='NG', reason='Testing error')
-        notifier = SlackNotifier(recipient)
-        notifier.slack = mock.MagicMock()
-        notifier.send(current_state)
-        assert notifier.slack.notify.called
-        _, called_kwargs = notifier.slack.notify.call_args
-        message = called_kwargs['text']
-        assert site.url in message
-        assert current_state.state in message
-
-    def test_notification_with_channel(self):
-        site = Site.objects.first()
-        recipient = SlackRecipient.objects.create(
-            site=site, url='http://example.com', channel='dummy')
-        current_state = mock.MagicMock(
-            site=site, state='NG', reason='Testing error')
-        notifier = SlackNotifier(recipient)
-        notifier.slack = mock.MagicMock()
-        notifier.send(current_state)
-        assert notifier.slack.notify.called
-        _, called_kwargs = notifier.slack.notify.call_args
-        assert 'channel' in called_kwargs
 
 
 class SlackRecipientDelete_ViewTest(ViewTestCase):
